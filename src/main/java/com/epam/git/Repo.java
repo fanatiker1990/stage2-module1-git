364c8e4514ea1591de0696c2a1022524571696ac
fanatiker1990 <59375292+fanatiker1990@users.noreply.github.com>
2023-05-29 16:42:23 +0300

    1

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,6 +1,6 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/fanatiker1990/git_task.git";
+    public static String REPO_LINK = "https://github.com/fanatiker1990/git_test.git";
 
 }
